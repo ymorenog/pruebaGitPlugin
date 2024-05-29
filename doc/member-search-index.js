ab7542557532b994f09daf0bdc8524dd1b65af6f
ymorenog <yolandamorenog@outlook.com>
2024-05-29 13:21:51 +0200

    prueba

---
+++ doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Prueba","l":"main(String[])","url":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"Prueba","l":"metodo()"},{"p":"<Unnamed>","c":"Prueba","l":"Prueba()","url":"%3Cinit%3E()"}]
--- src/Prueba.java
+++ src/Prueba.java
@@ -1,9 +1,22 @@
-
+/**
+ * Clase para probar
+ * @author ymoga
+ *
+ */
 public class Prueba {
-
+	/**
+	 * método principal para probar
+	 * @param args argumentos de entrada
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	/**
+	 * metodo para x
+	 */
+	public static void metodo() {
+		System.out.println("Hola");
 	}
 
 }
